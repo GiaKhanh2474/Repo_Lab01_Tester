c75ec6ca74639febcbe2985ca0c4451304cc3563
GiaKhanh2474 <khanh.2474802010185@vanlanguni.vn>
2026-09-16 16:13:49 +0700

    Add checkLogin function for user authentication

---
+++ login.js
@@ -0,0 +1,12 @@
+
+function checkLogin(username, password) {
+  if (username === 'admin' && password === '123') {
+    return true;
+  }
+  return false;
+}
+
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = checkLogin;
+}
